f4a48214ffa3e743fa57a3cd2bd99c22ba6424bd
Eddy <milkeddy1@dailyview.tw>
2023-10-10 19:03:02 +0800

    feat: env import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import EnvironmentPlugin from 'vite-plugin-environment'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  EnvironmentPlugin('all')],
+  server:{
+    port:3001
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
